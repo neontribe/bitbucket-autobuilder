48bcc9ebbf972621b0fa114d0e99c6e28f2b6d7d
Toby Batch <tobias@neontribe.co.uk>
2016-11-01 09:28:26 +0000

    Added create site on ticket number.

--- includes/uk/co/neontabs/bbucket/Makefile.php
+++ includes/uk/co/neontabs/bbucket/Makefile.php
@@ -15,10 +15,22 @@ class Makefile {
     }
   }
 
-  public function replace($needle, $new_version) {
+  function getMakefile() {
+    return $this->makefile;
+  }
+
+  function getLines() {
+    return $this->lines;
+  }
+
+  public function replace($needle, $new_version, $tag = TRUE) {
     $_needle = sprintf("projects[%s][download][tag]", $needle);
+    $type = $tag ? '[tag]' : '[branch]';
     foreach ($this->lines as $line_number => $line) {
       if (strpos($line, $_needle) === 0) {
+        if (!$tag) {
+          $_needle = str_replace('[tag]', '[branch]', $_needle);
+        }
         $this->lines[$line_number] = $_needle . ' = ' . $new_version . "\n";
       }
     }
