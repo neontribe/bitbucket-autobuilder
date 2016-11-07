b4a613a7521f80cb0a5bb7e130c8e49b9ba4e366
Toby Batch <tobias@neontribe.co.uk>
2016-11-07 15:30:37 +0000

    Starting on merge

--- pr_merge.php
+++ pr_merge.php
@@ -7,49 +7,27 @@ use uk\co\neontabs\bbucket\Makefile;
 include 'autoload.php';
 require_once __DIR__ . '/neontabs/nt_util/nt_util.opts.inc';
 
-$UID = time(TRUE);
-
-function debug_out($msg, $lvl = LOG_INFO) {
-  global $UID;
-
-  syslog($lvl, $UID . ': ' . $msg);
-  $strlvl = '';
-  switch ($lvl) {
-    case LOG_EMERG:
-      $strlvl = 'EMERG';
-      break;
-    case LOG_ALERT:
-      $strlvl = 'ALERT';
-      break;
-    case LOG_CRIT:
-      $strlvl = 'CRIT';
-      break;
-    case LOG_ERR:
-      $strlvl = 'ERR';
-      break;
-    case LOG_WARNING:
-      $strlvl = 'WARNING';
-      break;
-    case LOG_NOTICE:
-      $strlvl =  'NOTICE';
-      break;
-    case LOG_INFO:
-    default:
-      $strlvl = 'INFO';
-      break;
-    case LOG_DEBUG:
-      $strlvl = 'DEBUG';
-      break;
-  }
-  echo $strlvl .  ":\t" . $msg . "\n";
+function pr_debug($message) {
+  echo $message;
+  syslog(LOG_INFO, trim($message));
 }
 
+$UID = time(TRUE);
 
 $date = date('Y-m-d.H:i:s');
+$payload = file_get_contents('logs/pr-merge-2016-11-07.14:49:46.log');
+/*
 $payload = file_get_contents('php://input');
-$fname = "pr-merge-$date.log";
-debug_out(basename(__FILE__) . ': Writting data to ' . $fname);
+$fname = "logs/pr-merge-$date.log";
+pr_debug(basename(__FILE__) . ': Writting data to ' . $fname);
 $fh=fopen($fname, 'w');
 fwrite($fh, print_r($payload, TRUE));
 fclose($fh);
+*/
 
+$data = json_decode($payload, TRUE);
+$pull_request = $data['pullrequest'];
+$description = $pull_request['description'];
+
+if (strpos($description, 'BUILD:') !== FALSE) {
+}
